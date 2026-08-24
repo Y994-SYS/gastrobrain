ee734619caa6068334ea83c44e10c8bf5dd95d16
Alkan <github-alkanyasin3134@hotmail.com>
2026-08-24 19:13:11 +0300

    mail

--- backend/src/services/mail.service.js
+++ backend/src/services/mail.service.js
@@ -29,6 +29,7 @@ const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
     secure: false,
+    family: 4,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
